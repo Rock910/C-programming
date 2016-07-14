3ab53f080b64fd86f363441f25f1a095d4513fe7
Joshua Steier <josteier@optonline.net>
2016-07-14 13:16:06 -0500

    Update maxi.c

--- maxi.c
+++ maxi.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
+//num-> num
 // purpose: given three ints, print the max defining our own function
+// first step: delcare prototype
 //prototype
 int maxiumum(int x, int y, int z);
 int main (void){
@@ -13,7 +15,7 @@ int main (void){
     printf("Enter in value for c: ");
     scanf("%d", &c);
     printf("The maxiumum is %d", maxiumum(a,b,c));
-    
+    //second step: actually make the function
     
     
 }
@@ -25,7 +27,7 @@ int maxiumum(int x, int y, int z){
     }
     if (z<max){
         z = max;
-        
+        //be sure to use return, otherwise there will be no output
     }
     return max;
 }
